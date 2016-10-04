3b3aeb24a881f4fa7e314e8d8e0e4dcb0d31a230
mike <msr5zb@mail.missouri.edu>
2016-10-04 11:40:47 -0600

    Board Work

--- src/ai/homework4/FXMLDocumentController.java
+++ src/ai/homework4/FXMLDocumentController.java
@@ -11,6 +11,13 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.RowConstraints;
 
 /**
  *
@@ -18,35 +25,54 @@ import javafx.scene.control.Label;
  */
 public class FXMLDocumentController implements Initializable {
     
-    @FXML
     private Label label;
-    
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
-    }
+    private AnchorPane boardPane;
+    @FXML
+    private GridPane board;
+    
+
     
     @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        BoardStateSpace workingBoard = new BoardStateSpace();
+    public void initialize(URL url, ResourceBundle rb) {        
         
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+        int numCols = 4 ;
+        int numRows = 4 ;
+
+        for (int i = 0 ; i < numCols ; i++) {
+            ColumnConstraints colConstraints = new ColumnConstraints();
+            colConstraints.setHgrow(Priority.SOMETIMES);
+            board.getColumnConstraints().add(colConstraints);
+        }
+
+        for (int i = 0 ; i < numRows ; i++) {
+            RowConstraints rowConstraints = new RowConstraints();
+            rowConstraints.setVgrow(Priority.SOMETIMES);
+            board.getRowConstraints().add(rowConstraints);
+        }
+
+        for (int i = 0 ; i < numCols ; i++) {
+            for (int j = 0; j < numRows; j++) {
+                addPane(i, j);
+            }
+        }         
         
     }    
+
+       private void addPane(int colIndex, int rowIndex) {
+        Pane pane = new Pane();
+        pane.setOnMouseEntered(e -> {
+            System.out.printf("Mouse enetered cell [%d, %d]%n", colIndex, rowIndex);
+        });
+        board.add(pane, colIndex, rowIndex);
+    }
+    
+    private void handleBoardCellHover(MouseEvent event) {
+        
+        int row = 0;
+        int column = 0;
+        System.out.println("You Hovered Over: " + row + column);
+        
+    }
  
 }
